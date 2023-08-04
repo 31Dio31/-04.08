1a1797d657ce122675a71051ea48569c7850a7ea
Your Name <you@example.com>
2023-08-04 20:20:23 +0300

    задача выполнена

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int rannum=new Random().Next(10, 100);
+int num1 = rannum % 10;
+int num2 = rannum / 10;
+System.Console.WriteLine($"Выбрано число: {rannum}");
+System.Console.WriteLine (num1 > num2 ? num1 : num2);
+
